23a057562a87fb8597364849fdaba88634e80224
Valentina <Valentina_B@mail.ru>
2022-04-18 14:09:12 +0300

    Задача10

---
+++ Zadacha10/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+// int number1 = number / 10;//
+//int number2 = numberB % 10;//
+//Console.WriteLine(number2);//   - код без функции
+
+int SecondNumber(int number)
+{
+    int number2 = (number / 10) % 10;
+    return number2;
+}
+
+int result = SecondNumber(number);
+Console.WriteLine(result);
